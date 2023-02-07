a134dc809c6d1438f2a9d10030b1bbb1a3caaf4a
jcarvallo <jcarvallo.ar@gmail.com>
2023-02-07 10:12:17 -0300

    update dsnString

--- db/connection.go
+++ db/connection.go
@@ -15,8 +15,16 @@ var DSN string
 
 func DBConnection() {
 	var error error
-	dsnString := []string{"host=" + os.Getenv("HOST_DB"), "user=" + os.Getenv("USER_DB"), "password=" + os.Getenv("PASSWORD_DB"), "dbname=" + os.Getenv("NAME_DB"), "port=" + os.Getenv("PORT_DB"), "sslmode=" + os.Getenv("SSLMODE")}
+	var dsnString []string
+
+	dsnString = append(dsnString, "host="+os.Getenv("HOST_DB"))
+	dsnString = append(dsnString, "user="+os.Getenv("USER_DB"))
+	dsnString = append(dsnString, "password="+os.Getenv("PASSWORD_DB"))
+	dsnString = append(dsnString, "dbname="+os.Getenv("NAME_DB"))
+	dsnString = append(dsnString, "port="+os.Getenv("PORT_DB"))
+	dsnString = append(dsnString, "sslmode="+os.Getenv("SSLMODE"))
 	DSN = strings.Join(dsnString, " ")
+
 	DB, error = gorm.Open(postgres.Open(DSN), &gorm.Config{})
 	if error != nil {
 		log.Fatal(error)
